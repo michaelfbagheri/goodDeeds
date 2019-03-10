21d775e7b856a94401595de5365209280b3f410f
mikeB <mike.f.bagheri@gmail.com>
2019-03-10 15:31:25 -0400

    Use the express static middleware

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+app.use(express.static(path.join(__dirname, "../client/build")))
 
 router.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500
